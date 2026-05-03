915b60ce41b14484afee4ac3624941283c10c9f6
Abu <abu@abu-arfan.com>
2026-05-02 19:26:26 -0700

    Fix wrapper script paths for global npm installation

--- bin/ac.cmd
+++ bin/ac.cmd
@@ -4,9 +4,6 @@
 :: Get the directory where this script is located
 set "SCRIPT_DIR=%~dp0"
 
-:: Find the ac-compiler directory (go up from bin/ to project root)
-for %%I in ("%SCRIPT_DIR%..") do set "PROJECT_ROOT=%%~fI"
-set "AC_COMPILER_DIR=%PROJECT_ROOT%\ac-compiler"
-
-:: Run the AC compiler
-"%AC_COMPILER_DIR%\ac.exe" %*
+:: The ac binary is in the parent directory (npm flattens the structure)
+for %%I in ("%SCRIPT_DIR%..") do set "AC_COMPILER_DIR=%%~fI"
+"%AC_COMPILER_DIR%\ac-compiler\ac.exe" %*
